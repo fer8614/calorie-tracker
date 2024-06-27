1cdd69fec40ed0568cf7089bef9a234dad004548
fer8614 <fernando.inversionmf@gmail.com>
2024-06-27 09:48:04 -0700

    Creating Context for the global state and the Hook for the communication of the Context with the Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useReducer, useEffect, useMemo } from "react"
-import Form from "./components/Form"
-import { activityReducer, initialState } from "./reducers/activity-reducer"
+import { useEffect, useMemo } from "react";
+import Form from "./components/Form";
 import ActivityList from "./components/ActivityList";
 import CalorieTracker from "./components/CalorieTracker";
+import { useActivity } from "./hooks/useActivity";
 
 
 function App() {
 
-  const [ state, dispatch ] = useReducer( activityReducer, initialState )
+  const { state, dispatch } = useActivity()
 
   useEffect(() => {
     localStorage.setItem('activities', JSON.stringify(state.activities))
